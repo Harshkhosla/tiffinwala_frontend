4fc7d61eaa832dc16c00e7334f11d0123123d3d1
Harshkhosla <harshkhosla9945@gmail.com>
2024-11-05 01:01:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Banner from './component/Banner/Banner'
+import Footer from './component/Footer/Footer'
+import Header from './component/Header/GlassNav'
+import Hero from './component/Hero/Hero'
+import Newsletter from './component/Newsletter/Newsletter'
+import Stats from './component/Stats/Stats'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Banner/>
+    <Stats/>
+    <Newsletter/>
+   <Footer/>
+    </>
+  )
+}
+
+export default App
